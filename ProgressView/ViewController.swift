dfce5f1a91daed8cdae90888d83a08967b3454de
Carrington <Manyuchi>
2023-07-07 20:50:36 +0200

    progreessView

--- ProgressView/ViewController.swift
+++ ProgressView/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let coolProgressView: UIProgressView = {
+        let progressView = UIProgressView()
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        progressView.progressViewStyle = UIProgressView.Style.default
+        progressView.progress = 0.2
+        progressView.progressTintColor = UIColor.red
+        progressView.trackTintColor = UIColor.systemGray4
+        progressView.setProgress(0.4, animated: true)
+        return progressView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureViews()
+        configureConstraints()
     }
-
+    
+    private func configureViews() {
+        view.addSubview(coolProgressView)
+    }
+    
+    private func configureConstraints() {
+        let coolProgressViewConstraints = [
+            coolProgressView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+            coolProgressView.widthAnchor.constraint(equalToConstant: 280),
+            coolProgressView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(coolProgressViewConstraints)
+    }
 
 }
 
